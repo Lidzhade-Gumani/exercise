1e524aa595d81d553b2ae5c3b37aaf47514da1d4
22012897 <163580763+Lidzhade-Gumani@users.noreply.github.com>
2024-03-15 21:34:01 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+double calculateMarkup(double merchandiseCost, double employeesSalary, double yearlyRent, double electricityCost, double desiredProfit) {
+    double totalExpenses = merchandiseCost + employeesSalary + yearlyRent + electricityCost;
+    double grossProfit = totalExpenses / (1 - desiredProfit);
+    double markupPercentage = grossProfit / merchandiseCost;
+    return markupPercentage;
+}
+
+int main() {
+    double merchandiseCost, employeesSalary, yearlyRent, electricityCost, desiredProfit;
+
+    cout << "Enter the total cost of merchandise: ";
+    cin >> merchandiseCost;
+    cout << "Enter the total salary of employees (including your own salary): ";
+    cin >> employeesSalary;
+    cout << "Enter the yearly rent: ";
+    cin >> yearlyRent;
+    cout << "Enter the estimated electricity cost: ";
+    cin >> electricityCost;
+
+    desiredProfit = 0.10; // 10% desired net profit
+
+    double markupPercentage = calculateMarkup(merchandiseCost, employeesSalary, yearlyRent, electricityCost, desiredProfit);
+    double saleMarkupPercentage = markupPercentage / 0.85; // Taking into account the 15% sale
+
+    cout << "To achieve a net profit of approximately 10% after expenses and a 15% sale, markup the merchandise by " << (saleMarkupPercentage * 100) << "%." << endl;
+
+    return 0;
+}
